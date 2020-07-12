babbd4c927c4486b34e8a8af88f8e441a673d9da
Luísa Barros <qlullyq@gmail.com>
2020-07-12 16:48:39 -0300

    feat: add browser router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
-const App: React.FC = () => <Routes />;
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
