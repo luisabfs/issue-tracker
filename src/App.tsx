17ff57d3280adb44920eee173887b22e9f3ec98b
Luísa Barros <qlullyq@gmail.com>
2020-07-12 23:11:04 -0300

    style: create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './global/styles';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <GlobalStyles />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </>
 );
 
 export default App;
---
+++ src/global/styles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0%;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    background: #F5F7FA;
+    -webkit-font-smoothing: antialiased;
+    font: 16px Montserrat, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
