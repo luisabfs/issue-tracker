2ef372a06dc5fc7785cee73eb7861990378933ef
Luísa Barros <qlullyq@gmail.com>
2020-07-19 04:17:39 -0300

    refactor: add new components to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import Lottie from 'lottie-react';
-
-import animationData from 'assets/github.json';
 import logo from 'assets/logo.svg';
 
-import { Container, Wrapper, Logo, Title, Form } from './styles';
+import { Card, Header } from 'components';
+import { Container, Logo } from './styles';
 
 const Dashboard: React.FC = () => (
   <Container>
     <Logo src={logo} alt="Issue Tracker" />
-    <Wrapper>
-      <Container>
-        <Title>Contribute to open source projects!</Title>
-        <Form>
-          <input placeholder="Type the repo name" />
-          <button type="submit">Search</button>
-        </Form>
-      </Container>
-      <Container>
-        <Lottie animationData={animationData} loop={false} />
-      </Container>
-    </Wrapper>
+    <Header />
+    <Card />
   </Container>
 );
 
