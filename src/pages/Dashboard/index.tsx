5361fdc71e264f4af21835e31d2f6875b2f4a5fb
Luísa Barros <qlullyq@gmail.com>
2020-07-19 03:09:51 -0300

    feat: add input and button to search form

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,16 +4,22 @@ import Lottie from 'lottie-react';
 import animationData from 'assets/github.json';
 import logo from 'assets/logo.svg';
 
-import { Container, Wrapper, Title } from './styles';
+import { Container, Wrapper, Logo, Title, Form } from './styles';
 
 const Dashboard: React.FC = () => (
   <Container>
+    <Logo src={logo} alt="Issue Tracker" />
     <Wrapper>
-      <img src={logo} alt="" />
-      <Title>Contribute to open-source!</Title>
-    </Wrapper>
-    <Wrapper>
-      <Lottie animationData={animationData} loop={false} />
+      <Container>
+        <Title>Contribute to open source projects!</Title>
+        <Form>
+          <input placeholder="Type the repo name" />
+          <button type="submit">Search</button>
+        </Form>
+      </Container>
+      <Container>
+        <Lottie animationData={animationData} loop={false} />
+      </Container>
     </Wrapper>
   </Container>
 );
