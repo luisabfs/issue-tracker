e77af7a4e649d5f29fcb4ad9d7c094b9912c9d48
Luísa Barros <qlullyq@gmail.com>
2020-07-21 15:39:40 -0300

    feat: connect application to github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import Lottie from 'lottie-react';
+
 import logo from 'assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { RepoList, Header } from 'components';
-import { Container, Logo } from './styles';
+import animationData from 'assets/github.json';
+import api from 'services/api';
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <Logo src={logo} alt="Issue Tracker" />
-    <Header />
-    <RepoList />
-  </Container>
-);
+import { Container, Wrapper, Logo, Title, Form, Repositories } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState<string>();
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function addRepo(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const { data } = await api.get<Repository>(`repos/${newRepo}`);
+    setRepositories([...repositories, data]);
+
+    setNewRepo('');
+  }
+
+  return (
+    <Container>
+      <Logo src={logo} alt="Issue Tracker" />
+      <Wrapper>
+        <Container>
+          <Title>Contribute to open source projects!</Title>
+          <Form onSubmit={addRepo}>
+            <input
+              placeholder="Type the repo name"
+              value={newRepo}
+              onChange={event => setNewRepo(event.target.value)}
+            />
+            <button type="submit">Search</button>
+          </Form>
+        </Container>
+        <Container>
+          <Lottie animationData={animationData} loop={false} />
+        </Container>
+      </Wrapper>
+      <Repositories>
+        {repositories.map(repository => (
+          <a
+            key={repository.full_name}
+            href="https://github.com/facebook/react"
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={24} />
+          </a>
+        ))}
+      </Repositories>
+    </Container>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,9 +2,122 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
 `;
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+
 export const Logo = styled.img`
+  align-self: flex-start;
+`;
+
+export const Title = styled.h1`
+  color: #fafafa;
+  font-size: 48px;
+  margin-top: 40px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  max-width: 100%;
+  margin-top: 60px;
+
+  input {
+    flex: 1;
+    height: 50px;
+
+    border: 0;
+    color: #383838;
+    padding: 0 20px;
+    font-family: 'Montserrat', sans-serif;
+
+    border-radius: 50px 0 0 50px;
+  }
+
+  button {
+    width: 100px;
+    height: 50px;
+
+    color: #fafafa;
+    font-size: 16px;
+    font-weight: bold;
+    font-family: 'Montserrat', sans-serif;
+
+    border: 0;
+    background-color: #56d0c9;
+    border-radius: 0 50px 50px 0;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: #4bb8b2;
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  max-width: 650px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    background-color: #fafafa;
+    border-radius: 5px;
+    width: 100%;
+    padding: 20px;
+    text-decoration: none;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    &:focus {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 18px;
+        color: #383838;
+      }
+
+      p {
+        color: #a8a8a8;
+        font-size: 16px;
+        margin-top: 5px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #a8a8a8;
+    }
+  }
+`;
+
+export const Dots = styled.img`
+  margin-top: -40px;
+  margin-left: 20px;
   align-self: flex-start;
 `;
